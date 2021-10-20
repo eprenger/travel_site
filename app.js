fe7b7088529f79ef4e6a8b6a66d7a7aef7484e30
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-20 18:25:28 -0400

    toggle navbar / burger icon

--- app.js
+++ app.js
@@ -63,8 +63,9 @@ function animateSlides() {
   });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseText = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
@@ -80,14 +81,34 @@ function activeCursor(e) {
   if (item.classList.contains("explore")) {
     mouse.classList.add("explore-active");
     gsap.to(".title-swipe", 1, { y: "0%" });
-    mouseText.innerText = "Tap";
+    // mouseText.innerText = "Tap";
   } else {
     mouse.classList.remove("explore-active");
     gsap.to(".title-swipe", 1, { y: "100%" });
-    mouseText.innerText = "";
+    // mouseText.innerText = "";
   }
 }
 
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    gsap.to("#logo", 1, { color: "black" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    gsap.to("#logo", 1, { color: "white" });
+    document.body.classList.remove("hide");
+  }
+}
+
+//event listeners
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
 animateSlides();
