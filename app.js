626cd7520f984adad91cbd819108bc30737d5848
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-20 21:44:48 -0400

    bug fixes / mobile responsive css

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function animateSlides() {
     slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%" });
     slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideTl.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
-    slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+    // slideTl.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
 
     //create scene
     slideScene = new ScrollMagic.Scene({
@@ -130,12 +130,6 @@ barba.init({
       beforeEnter() {
         logo.href = "../index.html";
         detailAnimation();
-        gsap.fromTo(
-          ".nav-header",
-          1,
-          { y: "100%" },
-          { y: "0%", ease: "power2.inOut" }
-        );
       },
       beforeLeave() {
         controller.destory();
@@ -171,6 +165,13 @@ barba.init({
           { x: "100%", stagger: 0.25, onComplete: done }
         );
         tl.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+        tl.fromTo(
+          ".nav-header",
+          1,
+          { y: "-100%" },
+          { y: "0%", ease: "power2.inOut" },
+          " -=1.5 "
+        );
       },
     },
   ],
