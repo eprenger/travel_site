1d50737024ffba52f6fb43201e8af9963e97dc76
Evan Prenger <47509998+eprenger@users.noreply.github.com>
2021-10-20 21:26:28 -0400

    page transitions
    added page transitions from home page to fashion page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let controller;
 let slideScene;
 let pageScene;
+let detailScene;
 
 function animateSlides() {
   //init controller
@@ -128,12 +129,17 @@ barba.init({
       namespace: "fashion",
       beforeEnter() {
         logo.href = "../index.html";
+        detailAnimation();
         gsap.fromTo(
           ".nav-header",
           1,
           { y: "100%" },
           { y: "0%", ease: "power2.inOut" }
         );
+      },
+      beforeLeave() {
+        controller.destory();
+        detailScene.destory();
       },
     },
   ],
@@ -169,6 +175,33 @@ barba.init({
     },
   ],
 });
+
+function detailAnimation() {
+  controller = new ScrollMagic.Controller();
+  const slides = document.querySelectorAll(".detail-slide");
+  slides.forEach((slide, index, slides) => {
+    const slideTl = gsap.timeline({ defaults: { duration: 1 } });
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    const nextImg = nextSlide.querySelector("img");
+    slideTl.fromTo(slide, { opacity: 1 }, { opacity: 0 });
+    slideTl.fromTo(nextSlide, { opacity: 0 }, { opacity: 1 }, "-=1");
+    slideTl.fromTo(nextImg, { x: "50%" }, { x: "0%" });
+    //create scene
+    detailScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(slideTl)
+      // .addIndicators({
+      //   colorStart: "white",
+      //   colorTrigger: "white",
+      //   name: "detailScene",
+      // })
+      .addTo(controller);
+  });
+}
 
 //event listeners
 burger.addEventListener("click", navToggle);
